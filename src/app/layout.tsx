3c08991cdacce1395cfda0037050d2ec291575cd
Cguru <103155655+guyasong2@users.noreply.github.com>
2024-12-19 11:46:56 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
 import {
   ClerkProvider,
   SignedOut,
   SignedIn,
   UserButton,
-  SignInButton,
 } from "@clerk/nextjs";
 
 const geistSans = Geist({
